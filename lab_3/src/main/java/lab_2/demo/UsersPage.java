30802f4f621aaa2d45f470ffbe40d02642d3bc80
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-04-29 22:33:04 +0200

    Change repository file structure

---
+++ lab_3/src/main/java/lab_2/demo/UsersPage.java
@@ -0,0 +1,61 @@
+package lab_2.demo;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UsersPage {
+    private int pageNumber;
+    private int pagesCount;
+    private int pageSize;
+    private int totalCount;
+
+    private List<UserEntity> users;
+
+    public List<UserEntity> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserEntity> users) {
+        this.users = users;
+    }
+
+    public UsersPage(int pageNumber, int pagesCount, int pageSize, int totalCount, List<UserEntity> users) {
+        this.pageNumber = pageNumber;
+        this.pagesCount = pagesCount;
+        this.pageSize = pageSize;
+        this.totalCount = totalCount;
+        this.users = users;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public void setPagesCount(int pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public int getPagesCount() {
+        return pagesCount;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+}
---
+++ lab_3/src/main/java/lab_2/demo/UsersService.java
@@ -0,0 +1,38 @@
+package lab_2.demo;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class UsersService {
+
+    private ArrayList<UserEntity> users = new ArrayList<>();
+
+    @PostConstruct
+    private void onConstruct() {
+        this.users.add(new UserEntity(1,"Bartek","bartek@email.com")); //user1
+        this.users.add(new UserEntity(2,"Tomek","email@email.com")); //user2
+        this.users.add(new UserEntity(3,"Jan","email@email.com")); //user3
+    }
+
+    public UsersPage getUsers(int pageNumber, int pageSize){
+        pageNumber = Math.max(1,pageNumber);
+        pageSize = Math.max(1,pageSize);
+        pageSize = Math.min(100,pageSize);
+
+        int totalCount = this.users.size();
+        int pagesCount = (int)Math.ceil((double)totalCount/(double)pageSize);
+
+        return new UsersPage(pageNumber,pagesCount,pageSize,totalCount, this.users);
+    }
+
+    public void createUser(UserEntity user){
+        this.users.add(user);
+    }
+}
