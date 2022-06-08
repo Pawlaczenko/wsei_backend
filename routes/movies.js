3b25b1c4477cfb6a935c718d4e699356ed59dc75
Bartłomiej Pawlak <bartek.pawlak56@gmail.com>
2022-06-08 10:37:08 +0200

    Add movie delete and edit functionality

--- routes/movies.js
+++ routes/movies.js
@@ -45,14 +45,86 @@ router.post('/', async (req,res)=>{
 
     try {
         const newMovie = await movie.save();
-        //res.redirect(`movies/${newMovie.id}`);
-        res.redirect('movies');
+        res.redirect(`movies/${newMovie.id}`);
     } catch {
         renderNewPage(res,movie,true);
     }
 });
 
+router.get('/:id', async(req,res)=>{
+    try {
+        const movie = await Movie.findById(req.params.id).populate('director').exec();
+        res.render('movies/show',{movie:movie});
+    } catch {
+        res.redirect('/');
+    }
+});
+
+
+// Edit Movie
+router.put('/:id', async (req,res)=>{
+    let movie;
+
+    try {
+        movie = await Movie.findById(req.params.id);
+        movie.title = req.body.title;
+        movie.director = req.body.director;
+        movie.releaseDate = new Date(req.body.releaseDate);
+        movie.duration = req.body.duration;
+        movie.description = req.body.description;
+        if(req.body.poster != null && req.body.poster !== ''){
+            savePoster(movie, req.body.poster);
+        }
+        await movie.save();
+        res.redirect(`/movies/${movie.id}`);
+    } catch(error) {
+        console.log(error);
+        if(movie != null){
+            renderEditPage(res,movie,true);
+        } else {
+            res.redirect('/');
+        }
+            
+    }
+});
+
+router.get('/:id/edit', async (req, res)=>{
+    try {
+        const movie = await Movie.findById(req.params.id);
+        renderEditPage(res,movie);
+    } catch {
+        res.redirect('/');
+    }
+});
+
+//Delete movie Page
+router.delete('/:id', async (req, res)=>{
+    let movie;
+    try {
+        movie = await Movie.findById(req.params.id);
+        await movie.remove();
+        res.redirect('/movies');
+    } catch {
+        if(movie != null){
+            res.render('movies/show', {
+                movie: movie,
+                errorMessage: 'Could not remove movie'
+            });
+        } else {
+            res.redirect('/');
+        }
+    }
+});
+
 const renderNewPage = async (res, movie, hasError = false ) => {
+    renderFormPage(res,movie,'new',hasError);
+}
+
+const renderEditPage = async (res, movie, hasError = false ) => {
+    renderFormPage(res,movie,'edit',hasError);
+}
+
+const renderFormPage = async (res, movie, form, hasError = false ) => {
     try {
         const directors = await Director.find({});
         const params = {
@@ -60,7 +132,7 @@ const renderNewPage = async (res, movie, hasError = false ) => {
             movie: movie
         }
         if(hasError) params.errorMessage = 'Error Creating a Movie';
-        res.render('movies/new',params);
+        res.render(`movies/${form}`,params);
     } catch {
         res.redirect('/movies');
     }
